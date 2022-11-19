43d500750e678c0967dcdfc14b9cd2c27720891b
David Mello <davidbezerra10@hotmail.com>
2022-11-19 18:05:16 -0300

    Adicionei a criação do elemento mosquito e a funcionalidade móvel do mosquito
    Adicionei a criação do elemento mosquito e funções randômicas para a mobilidade do mosquito.

--- jogo.js
+++ jogo.js
@@ -8,4 +8,38 @@ function ajustaTamanhoPalcoJogo() {
     console.log(altura, largura);
 }
 
-ajustaTamanhoPalcoJogo()
+ajustaTamanhoPalcoJogo();
+
+function posicaoRandomica() {
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY);
+
+    //criação do elemento html
+    var mosquito = document.createElement('img');
+    mosquito.src = "imagens/mosquito.png";
+    mosquito.className = tamanhoAleatorio();
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito);
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
