ae9cd9fe2d01bb60f00f80ac57c358f71037c645
David Mello <davidbezerra10@hotmail.com>
2022-11-19 20:15:03 -0300

    Game Over
    Adicionei a lógica do Game Over e criei a página de fim de jogo com o botão  de reinicialização.

--- jogo.js
+++ jogo.js
@@ -18,7 +18,7 @@ function posicaoRandomica() {
         document.getElementById("mosquito").remove();
 
         if (vidas > 3) {
-            alert("GAME OVER");
+            window.location.href = 'fim_de_jogo.html'
         } else {
             document.getElementById("v" + vidas).src =
                 "imagens/coracao_vazio.png";
