1eb17a254c8dfba93061fb1964fb36faaa875b5b
David Mello <davidbezerra10@hotmail.com>
2022-11-19 21:01:23 -0300

    Cronômetro
    Implementei o cronômetro do jogo, atuando no tempo necessário para perder ou ganhar a partida.

--- jogo.js
+++ jogo.js
@@ -2,6 +2,7 @@ var altura = 0;
 var largura = 0;
 
 var vidas = 1;
+var tempo = 15
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
@@ -11,6 +12,18 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo();
+
+var cronometro = setInterval(function() {
+    tempo -= 1
+
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('Vitória')
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+}, 1000)
 
 function posicaoRandomica() {
     //remover o mosquito caso ele exista
